e721c7164b6b5c9fb90cf22344e7768d66eeef84
Ammar Waleed <ongo9972@gmail.com>
2026-04-30 06:30:14 -0600

    Update solution.js
    Adding The Solution

--- LeetCode/Easy/01-Two-Sum/solution.js
+++ LeetCode/Easy/01-Two-Sum/solution.js
@@ -1,11 +1,9 @@
-# 01 - Two Sum
 
-## Problem Description
-Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
-
-## Approach
-Used a **Hash Map** to store values and their indices. This allows us to find the complement in $O(1)$ time.
-
-## Complexity
-- **Time Complexity:** $O(n)$
-- **Space Complexity:** $O(n)$
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) return [map.get(complement), i];
+        map.set(nums[i], i);
+    }
+}

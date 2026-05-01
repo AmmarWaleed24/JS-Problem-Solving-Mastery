fb8180e9ca7e8a50b8c52e1469ce4467c936a72a
Ammar Waleed <ongo9972@gmail.com>
2026-05-01 14:39:53 -0600

    Create solution.js
    [Add the 20-th problem description/solution]

---
+++ LeetCode/Easy/20-Valid-Palindrome/solution.js
@@ -0,0 +1,10 @@
+var isPalindrome = function(s) {
+    let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let left = 0, right = cleaned.length - 1;
+    while (left < right) {
+        if (cleaned[left] !== cleaned[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+};

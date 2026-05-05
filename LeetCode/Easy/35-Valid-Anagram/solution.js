342be7c529e89d502c4a21182479e9847ed56be1
Ammar Waleed <ongo9972@gmail.com>
2026-05-05 11:55:36 -0600

    Create solution.js
    Add The Problem Solution

---
+++ LeetCode/Easy/35-Valid-Anagram/solution.js
@@ -0,0 +1,10 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let counts = {};
+    for (let char of s) counts[char] = (counts[char] || 0) + 1;
+    for (let char of t) {
+        if (!counts[char]) return false;
+        counts[char]--;
+    }
+    return true;
+};

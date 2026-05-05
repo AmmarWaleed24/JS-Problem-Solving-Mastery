e1d1b141ce17b7eeb0753faa9583b61ab4fc8c84
Ammar Waleed <ongo9972@gmail.com>
2026-05-05 11:54:52 -0600

    Create solution.js
    Add The Problem Solution

---
+++ LeetCode/Easy/34-Missing-Number/solution.js
@@ -0,0 +1,6 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = nums.reduce((acc, curr) => acc + curr, 0);
+    return expectedSum - actualSum;
+};
